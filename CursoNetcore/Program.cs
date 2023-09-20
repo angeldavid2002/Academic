a0a2b98b3725b8ab6e42b0aed3d3ecc2c6b0c592
angeldavid2002 <70535423+angeldavid2002@users.noreply.github.com>
2023-09-20 12:27:57 -0500

    commit inicial

---
+++ CursoNetcore/Program.cs
@@ -0,0 +1,9 @@
+﻿int year = 2023;
+float since = 4.2F;
+double age = 21.9;
+decimal semester = 9.05M;
+String carrer = "System Engineer";
+String name = "Angel Bolaño";
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I'm " + name + ", today is {0} and i'm {1} years old, i'm studying {2} in the University Popular of Cesar " + "i stay in {3} semester but i'm learnig to programation since {4} semester", year, age, carrer, semester, since);
+Console.ReadLine();
