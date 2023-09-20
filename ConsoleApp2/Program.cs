1becfb117d36362bdb33be4dbb96fbb3a20f5eb2
angeldavid2002 <70535423+angeldavid2002@users.noreply.github.com>
2023-09-20 14:59:49 -0500

    segundo archivo de consola

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,9 @@
+﻿int num1 = 21;
+int num2 = 17;
+
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multi = num1 * num2;
+var div = num1 / num2;
+
+Console.WriteLine(" Suma: {0}\n Resta: {1}\n Multiplicacion: {2}\n Division: {3}", suma, resta, multi, div);
--- CursoNetcore/Program.cs
+++ CursoNetcore/Program.cs
@@ -2,8 +2,10 @@
 float since = 4.2F;
 double age = 21.9;
 decimal semester = 9.05M;
+string ty = "hola";
 String carrer = "System Engineer";
 String name = "Angel Bolaño";
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I'm " + name + ", today is {0} and i'm {1} years old, i'm studying {2} in the University Popular of Cesar " + "i stay in {3} semester but i'm learnig to programation since {4} semester", year, age, carrer, semester, since);
+Console.WriteLine(ty);
 Console.ReadLine();
