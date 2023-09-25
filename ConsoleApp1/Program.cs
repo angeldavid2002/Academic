785a808322bd53fdecf6dbc5490b0b94af311c91
angeldavid2002 <70535423+angeldavid2002@users.noreply.github.com>
2023-09-25 14:49:28 -0500

    ultimos cambios

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] array = new string[5];
+array[0] = "Angel";
+array[1] = "David";
+array[2] = "Bolaño";
+array[3] = "Course";
+array[4] = "C#";
+
+string[] name = { "Angel", "David", "Bolaño", "Course", "C#" };
+
+int[] age = new int[5];
+
+age[0] = 5;
+age[1] = 10;
+age[2] = 21;
+age[3] = 38;
+age[4] = 99;
+
+int[] ages = { 8, 90, 21, 10, 5 };
+
+Console.WriteLine("Resultado: {0}", array[0] + " " + name[3] + "\nAge: " + ages[2]);
+
+
+Console.ReadLine();
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,6 @@
-﻿int num1 = 21;
+﻿using System.Xml.Linq;
+
+int num1 = 21;
 int num2 = 17;
 
 var suma = num1 + num2;
@@ -6,4 +8,45 @@ var resta = num1 - num2;
 var multi = num1 * num2;
 var div = num1 / num2;
 
+int val1 = 9;
+var val2 = 9;
+double val3 = 10;
+double val4 = 10;
+var data = val1 != val2;
+string name;
+
 Console.WriteLine(" Suma: {0}\n Resta: {1}\n Multiplicacion: {2}\n Division: {3}", suma, resta, multi, div);
+
+if (val1 != val2 || val3 == val4) //Nos da el resultado si es verdad cualquiera de las 2 condiciones
+{
+    Console.WriteLine("Resultado: {0}", data);
+}
+if (data)           //si es verdadera la condición
+{
+    Console.WriteLine("\nResultado True: {0}\n", data);
+    if (val3 == val4)
+    {
+        Console.WriteLine("La condicional es verdadera y la opereacion es verdadera");
+    }
+    else
+    {
+        Console.WriteLine("La condicional es verdadera y la operacion es falsa");
+    }
+}
+else            //sino es verdadera la condición
+{
+    Console.WriteLine("\nResultado False: {0}\n", data);
+    if (val3 == val4)
+    {
+        Console.WriteLine("La condicional es falsa y la opereacion es verdadera");
+    }
+    else
+    {
+        Console.WriteLine("La condicional es falsa y la operacion es falsa");
+    }
+}
+
+name = data ? "Angel" : "Bolaño";
+Console.WriteLine("data: "+name);
+
+
